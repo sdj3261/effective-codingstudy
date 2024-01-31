00bf89c739c729d6d6defbbb216bbb052cc25ae6
DongJae <tlsehdwo7@naver.com>
2024-02-01 02:46:35 +0900

    init

---
+++ src/main/java/com/dongjae/dev/effectivecodingstudy/EffectiveCodingstudyApplication.java
@@ -0,0 +1,13 @@
+package com.dongjae.dev.effectivecodingstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EffectiveCodingstudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EffectiveCodingstudyApplication.class, args);
+	}
+
+}
