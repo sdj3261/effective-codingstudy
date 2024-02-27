aea67cc995a0025e6028482f78b7017ab2c978cb
DongJae <tlsehdwo7@naver.com>
2024-02-27 11:04:56 +0900

    fix : @Entity , @Table 어노테이션 추가

--- src/main/java/com/dongjae/dev/effectivecodingstudy/domain/Problem.java
+++ src/main/java/com/dongjae/dev/effectivecodingstudy/domain/Problem.java
@@ -1,9 +1,12 @@
 package com.dongjae.dev.effectivecodingstudy.domain;
 
+import static com.dongjae.dev.effectivecodingstudy.common.Const.ProblemType;
 import com.dongjae.dev.effectivecodingstudy.common.Const;
 import com.dongjae.dev.effectivecodingstudy.common.model.BaseEntity;
 import jakarta.persistence.*;
 
+@Entity
+@Table(name = "problems")
 public class Problem extends BaseEntity {
     @EmbeddedId
     private ProblemId id;
@@ -12,7 +15,7 @@ public class Problem extends BaseEntity {
     private String description;
 
     @Enumerated(EnumType.STRING)
-    private Const.ProblemType type; // Const 클래스의 내부 열거형 사용
+    private ProblemType type; // Const 클래스의 내부 열거형 사용
 
     @ManyToOne
     private User user;
@@ -20,6 +23,11 @@ public class Problem extends BaseEntity {
     public Problem(ProblemId problemId) {
         this.id = problemId;
     }
+
+    public Problem() {
+
+    }
+
     public static Problem create() {
         return new Problem(ProblemId.generate());
     }
